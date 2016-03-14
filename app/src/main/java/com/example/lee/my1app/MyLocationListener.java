8e70cb9070761d82659766da40564d93e9c6dc63
lee <tn3728@naver.com>
2016-03-14 10:05:57 +0900

    위치

---
+++ app/src/main/java/com/example/lee/my1app/MyLocationListener.java
@@ -0,0 +1,32 @@
+package com.example.lee.my1app;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+
+public class MyLocationListener implements LocationListener{
+    public double latitude, longitude, altitude;
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        altitude = location.getAltitude();
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
